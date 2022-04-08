8c77f5106e54e2f3a50ccebc9f86626eb48bca9f
KhaiNguyen <duckhailinux@gmail.com>
2022-04-08 13:58:40 +0700

    add register page

---
+++ FootWearStore/store/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from matplotlib import widgets
+class LoignForm(UserCreationForm):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class':'form-control'}),
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class':'form-control'}),
+    )
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username','email','password1','password2']
+        widgets={
+            'first_name': forms.TextInput(attrs={'class': 'form-control' }),
+            'last_name':  forms.TextInput(attrs={'class': 'form-control' }),
+            'email': forms.EmailInput(attrs={'class': 'form-control' }),
+            'username':forms.TextInput(attrs={'class': 'form-control' }),
+        }
--- FootWearStore/store/views.py
+++ FootWearStore/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+from .forms import LoignForm
 # Create your views here.
 def index(request):
     products = models.Product.objects.all()
@@ -22,14 +24,23 @@ def product_detail(request, slug):
         'pro_colors': pro_colors,
         'sizes': list_size,
     })
+def register(request):
+    form = LoignForm()
+    if request.method == "POST":
+        form = LoignForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form':form}
+    return render(request,'store/register.html',context)
+
 def cart(request):
     return render(request,'store/cart.html')
 def store(request):
     return render(request,'store/store.html')
 def login(request):
     return render(request,'store/signin.html')
-def register(request):
-    return render(request,'store/register.html')
+
 def place_order(request):
     return render(request,'store/place-order.html')
 def order_complete(request):
